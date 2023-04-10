80a121b19687e3871304c02aa4a2175658d29384
Cameron Mukherjee <cameron@hexploits.com>
2023-04-10 21:50:11 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+// This project should locate the 'weight' for each file in a project and genereate a report.
+// This weight can be for files.
+
+// Weight is dependent on lines of code, amount of for loops, if statements, and other tokens.
+// line of code = 1 point
+// for loop = 2 points
+// if statement = 3 points
+// the above is configurable via ~/.w8/config.yml
+
+fn main() {
+
+    println!("Hello, world!");
+}
